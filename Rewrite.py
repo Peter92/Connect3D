f68d599bcee8f1e2d1cab513a138ab15e7e761bc
Peter <Peter92@users.noreply.github.com>
2016-02-17 13:35:04 +0000

    Split game related functions into separate class

--- Rewrite.py
+++ Rewrite.py
@@ -7,22 +7,14 @@ class Connect3D(object):
     Supports numbers up to 255.
     """
     DEFAULT_SIZE = 4
-    DEFAULT_PLAYERS = 2
-    DEFAULT_SHUFFLE_TURNS = 3
     DEFAULT_SHUFFLE_LEVEL = 1
     
-    def __init__(self, size=None, players=DEFAULT_PLAYERS, shuffle=None, shuffle_turns=None):
+    def __init__(self, size=None, shuffle_level=DEFAULT_SHUFFLE_LEVEL):
         
         self.size = self.DEFAULT_SIZE if size is None else max(1, size)
-        self.num_players = self.DEFAULT_PLAYERS if players is None else max(1, players)
-        self.shuffle_level = self.DEFAULT_SHUFFLE_LEVEL if shuffle is None else max(0, min(2, shuffle))
-        self.shuffle_turns = self.DEFAULT_SHUFFLE_TURNS if shuffle_turns is None else max(0, shuffle_turns)
+        self.shuffle_level = self.DEFAULT_SHUFFLE_LEVEL if shuffle_level is None else max(0, min(2, shuffle_level))
         
-        try:
-            self._player
-        except AttributeError:
-            self._player = random.randint(1, self.num_players)
-        
+        #Precalculated parts
         self._size_squared = int(pow(self.size, 2))
         self._size_cubed = int(pow(self.size, 3))
         self._range_sm = range(self.size)
@@ -30,6 +22,7 @@ class Connect3D(object):
         self._range_md = range(self._size_squared)
         self._range_lg = range(self._size_cubed)
         
+        #Main parts
         self.range = bytearray(self._range_lg)
         self.grid = bytearray(0 for _ in self.range)
         self.flip = FlipGrid(self)
@@ -56,24 +49,20 @@ class Connect3D(object):
     def __repr__(self):
         output = (bytearray([self._player]) + bytearray([self.num_players])
                   + self.grid + self.range + bytearray([':']) + self.progress)
-        return "Connect3D.load('{}')".format(base64.b64encode(output))
+        return "Connect3DGame.load('{}')".format(base64.b64encode(output))
 
     def __str__(self):
         """Print the current state of the grid."""
-        
         grid_range = range(self.size)
         grid_output = []
-        
         k = 0
         for j in grid_range:
-            
             row_top = ' ' * (self.size * 2 + 1) + '_' * (self.size * 4)
             if j:
                 row_top = '|{}|{}|{}|'.format(row_top[:self.size * 2 - 1],
                                               '_' * (self.size * 2),
                                               '_' * (self.size * 2 - 1))
             grid_output.append(row_top)
-            
             for i in grid_range:
                 row_display = '{}/{}'.format(' ' * (self.size * 2 - i * 2), 
                                              ''.join(('{}{}{}/'.format('' if self.grid[k + x] > 9 else ' ', 
@@ -97,6 +86,59 @@ class Connect3D(object):
                 
         return '\n'.join(grid_output)
 
+
+    def shuffle(self, level=None):
+        """Mirror the grid in the X, Y, or Z axis.
+        
+        A level of 1 is mirror only.
+        A level of 2 includes rotation.
+        """
+        if level is None:
+            level = self.shuffle_level
+            
+        #Shuffle is disabled
+        if not level and isinstance(level, bool):
+            return
+            
+        max_shuffles = level * 3
+        shuffles = random.sample(range(max_shuffles), random.randint(0, max_shuffles - 1))
+        
+        all_flips = (self.flip.fx, self.flip.fy, self.flip.fz, 
+                     self.flip.rx, self.flip.ry, self.flip.rz, self.flip.reverse)
+        
+        for i in shuffles:
+            self.grid = all_flips[i](self.grid)
+            self.range = all_flips[i](self.range)
+
+    def load_grid(self, grid):
+        grid = bytearray(grid)
+        if len(grid) != self._size_cubed:
+            raise ValueError("grid length must be '{}' not '{}'".format(self._size_cubed, len(grid)))
+        self.grid = grid
+        
+
+class Connect3DGame(object):
+    DEFAULT_PLAYERS = 2
+    DEFAULT_SHUFFLE_TURNS = 3
+    
+    def __init__(self, num_players=DEFAULT_PLAYERS, shuffle_turns=DEFAULT_SHUFFLE_TURNS, size=None):
+        self.num_players = self.DEFAULT_PLAYERS if num_players is None else max(1, num_players)
+        self.shuffle_turns = self.DEFAULT_SHUFFLE_TURNS if shuffle_turns is None else max(0, shuffle_turns)
+        self.core = Connect3D(size)
+        
+        try:
+            self._player
+        except AttributeError:
+            self._player = random.randint(1, self.num_players)
+        
+    def next_player(self):
+        self._player += 1
+        self._player %= self.players
+    
+    def previous_player(self):
+        self._player -= 1
+        self._player %= self.players
+        
     @classmethod
     def load(cls, data):
         """Calculate the number of segments and split the data into the correct sections.
@@ -106,10 +148,7 @@ class Connect3D(object):
                 Must be a base 64 encoded bytearray.
                 Acceptable inputs:
                     bytearray(grid_data)
-                    bytearray(current_player + num_players + grid_data + range_data + progress_data)
-        
-        Output:
-            (num_segments, current_player, grid_data, range_data, progress_data)
+                    bytearray(current_player + num_players + grid_data + range_data)
         """
         if not isinstance(data, (bytearray, str)):
             raise ValueError("'{}' input must be a 'bytearray'".format(type(data).__name__))
@@ -143,36 +182,13 @@ class Connect3D(object):
             data_range = data[data_length + 1:data_length * 2]
         
         #Create new class
-        new_instance = cls(size=int(cube_root), players=data_players)
+        new_instance = cls(size=int(cube_root), num_players=data_players)
         if data_players is not None:
             new_instance._player = data_players
-        new_instance.grid = data_grid
-        new_instance.range = data_range
+        new_instance.core.grid = data_grid
+        new_instance.core.range = data_range
         return new_instance
-    
-    def shuffle(self, level=None):
-        """Mirror the grid in the X, Y, or Z axis.
         
-        A level of 1 is mirror only.
-        A level of 2 includes rotation.
-        """
-        if level is None:
-            level = self.shuffle_level
-            
-        #Shuffle is disabled
-        if not level and isinstance(level, bool):
-            return
-            
-        max_shuffles = level * 3
-        shuffles = random.sample(range(max_shuffles), random.randint(0, max_shuffles - 1))
-        
-        all_flips = (self.flip.fx, self.flip.fy, self.flip.fz, 
-                     self.flip.rx, self.flip.ry, self.flip.rz, self.flip.reverse)
-        
-        for i in shuffles:
-            self.grid = all_flips[i](self.grid)
-            self.range = all_flips[i](self.range)
-            
 
 def split_list(x, n):
     """Split a list by n characters."""
@@ -262,6 +278,6 @@ class FlipGrid(object):
         return data[::-1]
         
 
-c = Connect3D.load(bytearray(range(27)))
-c.shuffle()
-print c
+c = Connect3DGame.load(bytearray(range(27)))
+c.core.shuffle()
+print c.core
